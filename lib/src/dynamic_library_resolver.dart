0d92f6d206b11fd5e36cc57eb916de4e3132a195
zubithedev <zubair1234566@gmail.com>
2024-09-05 17:26:05 +0500

    Initial Commit

---
+++ lib/src/dynamic_library_resolver.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+import 'dart:io';
+
+/// Resolves dynamic library for FFI depending on current platform.
+DynamicLibrary resolveDynamicLibrary() {
+  if (Platform.isLinux) {
+    return DynamicLibrary.open('h3.so');
+  }
+  if (Platform.isAndroid) {
+    return DynamicLibrary.open('libh3.so');
+  }
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('h3.dll');
+  }
+  return DynamicLibrary.process();
+}
